22ffa5dc8b67d3fc536b3d2ff0f0f18b4e95d642
Iv4nyi <99827041+Iv4nyi@users.noreply.github.com>
2024-04-22 22:31:29 +0200

    Fizetes oldal (#1)

---
+++ src/components/payment/Success.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Success.css';
+
+function Success() {
+  return (
+    <div className="success-container">
+      <div className="header">
+        <div className="title">Sikeres megrendelés</div>
+        <div className="subtitle">Rendelése száma: 256</div>
+      </div>
+    </div>
+  );
+}
+
+export default Success;
