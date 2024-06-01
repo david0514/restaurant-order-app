94221996ad28eedab6619418fbe8cd6aebf21e05
Ócsai Dávid <ocsai.david@gmail.com>
2024-06-01 12:52:23 +0200

    feat: final program v1.1

--- src/components/top-bar/TopBar.tsx
+++ src/components/top-bar/TopBar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './TopBar.css';
+import {useNavigate} from "react-router-dom";
 
 function TopBar() {
 
     const isLoggedIn = false
 
+    const navigate = useNavigate();
+
     return (
         <div className="top-bar-container">
-            <div className="top-bar-left-part">
+            <div className="top-bar-left-part" onClick={() => navigate('/', {replace: true})}>
                 <img className="restaurant-logo" src="/top-bar/restaurant-logo.svg" alt=""/>
                 <span className="restaurant-name">Teszt étterem</span>
             </div>
@@ -16,7 +19,9 @@ function TopBar() {
             </div>
             <div className="top-bar-right-part">
                 {/*<button className="outline-button outline-button-light" style={{marginRight: "0.5rem"}}>Rendelés</button>*/}
+
                 <img className="user-icon" src="/top-bar/user-icon.svg" alt=""/>
+
                 {/*{isLoggedIn ?*/}
                 {/*    <img className="logout-icon" src="/top-bar/logout-icon.svg" alt=""/> :*/}
                 {/*    <img className="login-icon" src="/top-bar/login-icon.svg" alt=""/>*/}
